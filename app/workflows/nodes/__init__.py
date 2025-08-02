6e4d0bd79732d7705ac3097a0bb42e4695822ea2
nicholasbay <bay.yong.wei@gmail.com>
2025-08-02 13:39:45 +0800

    Remove import of ChunkRetriever from workflow nodes

--- app/workflows/nodes/__init__.py
+++ app/workflows/nodes/__init__.py
@@ -1,4 +1,3 @@
-from .chunk_retriever import ChunkRetriever
 from .chunk_summarizer import ChunkSummarizer
 from .history_fetcher import HistoryFetcher
 from .response_generator import ResponseGenerator
